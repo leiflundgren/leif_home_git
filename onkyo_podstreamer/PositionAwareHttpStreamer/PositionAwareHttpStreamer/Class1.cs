6523b712d1ecc8839f2f6b9d53aa081e500685c6
Leif Lundgren <leiflundgren@leiflundgren.com>
2014-03-11 20:45:29 +0100

    small restricted server which attemts to play several files.

---
+++ onkyo_podstreamer/PositionAwareHttpStreamer/PositionAwareHttpStreamer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositionAwareHttpStreamer
+{
+    public class Class1
+    {
+    }
+}
---
+++ onkyo_podstreamer/change_tune_test/src/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace change_tune_test
+{
+    class Program
+    {
+        static Stream mp3stream;
+        static Stream outstream;
+        static List<string> files;
+        static int file_pos;
+        static string dir = @"C:\Users\leif\Music";
+
+        static void Main(string[] args)
+        {
+
+
+            HttpListener server = new HttpListener();
+            server.Prefixes.Add("http://192.168.3.161:8091/");
+            server.Start();
+            var ctx = server.GetContext();
+            files = new List<string>(Directory.EnumerateFiles(dir, "*.mp3"));
+            file_pos = 0;
+            
+            PushData();
+
+            while ( file_pos < files.Count)
+            {
+                Console.Out.WriteLine("Playing " + files[file_pos] + " " + file_pos + " of  " + files.Count + ". Enter to skip ahead");
+                Console.In.Read();
+                ++file_pos;
+                mp3stream = null;
+            }
+            System.Threading.Thread.Sleep(5000);
+        }
+
+        private static void PushData()
+        {
+            if ( mp3stream == null )
+            {
+                if (file_pos >= files.Count)
+                    return;
+                mp3stream = new FileStream(Path.Combine(dir, files[file_pos]), FileMode.Open, FileAccess.Read);
+            }
+
+            var buf = new byte[1024];
+            int len = mp3stream.Read(buf, 0, buf.Length);
+
+            Console.WriteLine("Sending " + len + " bytes");
+            outstream.BeginWrite(buf, 0, len, data_sent, null);
+        }
+
+        private static void data_sent(IAsyncResult ar)
+        {
+            PushData();
+        }
+
+    }
+}
